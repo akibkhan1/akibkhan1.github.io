50e10f9f3fbe4642fa89a6ce2591dc46409786dc
akibkhan1 <akibmohammed@iut-dhaka.edu>
2025-06-01 21:49:58 +0000

    Deploying to gh-pages from @ akibkhan1/akibkhan1.github.io@be607581381d3e9b7452e36c8830e01cbf8a8a0f 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -433,16 +433,16 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/the_godfather/";
-            },},{id: "news-a-simple-inline-announcement",
-          title: 'A simple inline announcement.',
-          description: "",
-          section: "News",},{id: "news-a-long-announcement-with-details",
+            },},{id: "news-a-long-announcement-with-details",
           title: 'A long announcement with details',
           description: "",
           section: "News",handler: () => {
               window.location.href = "/news/announcement_2/";
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
+          description: "",
+          section: "News",},{id: "news-joined-bangladesh-university-of-businesss-and-technology-bubt-as-a-faculty-member-for-the-cse-dept",
+          title: 'Joined Bangladesh University of Businesss and Technology (BUBT) as a faculty member for...',
           description: "",
           section: "News",},{id: "projects-project-1",
           title: 'project 1',
